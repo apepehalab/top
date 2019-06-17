2b5bc19ee69714f2be7dc0914894ec253767cf7c
Avicus Delacroix <avicus@ya.ru>
2019-06-17 12:55:27 +0300

    Initial commit of controls and camera scripts

---
+++ alControls.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class alControls : MonoBehaviour
+{
+
+    public Camera cam; // камера
+    private Vector3 offset; // смещение камеры относительно объекта
+   // public float camDistance = 10f; //отдаление камеры
+    public Rigidbody rig; //физика объекта
+    private float speed = 7f; // скорость
+    private float vel = 0f; // модуль вектора скорости
+    private Vector3 movement = new Vector3(0, 0, 0); // трёхмерный вектор скорости
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");// принимает значения от -1 до 1 соответственно s w и ↓ ↑
+        float horizontal = Input.GetAxis("Horizontal");// принимает значения от -1 до 1 соответственно a d и ← →
+
+        float ang = rig.transform.eulerAngles.y * Mathf.PI / 180; //угол поворота rigidbody в радианах
+
+        Vector3 horMove = new Vector3(0, 0, 0); // трёхмерный вектор для горизонтальной компоненты движения
+        Vector3 verMove = new Vector3(0, 0, 0); // трёхмерный вектор для вертикальной компоненты движения
+        
+
+        Quaternion angvelR = Quaternion.Euler(new Vector3(0, 100, 0) * Time.deltaTime); //угловая скорость поворота вправо
+        Quaternion angvelL = Quaternion.Euler(new Vector3(0, -100, 0) * Time.deltaTime); //угловая скорость поворота влево
+
+        if (horizontal != 0) // проверка горизонтальной оси
+        {
+            if (horizontal > 0) // если нажата кнопка вправо
+            {
+               vel = speed; //модуль вектора скорости равен скорости
+            }
+
+            else if (horizontal < 0)// если нажата кнопка влево
+            {
+                vel = -speed; //модуль вектора скорости равен -скорости
+            }
+            horMove = new Vector3(-vel * (float)Math.Cos(ang + Mathf.PI / 2), 0, vel * (float)Math.Sin(ang + Mathf.PI / 2)) * Time.deltaTime; //вчислеие трёхмерной горизонтальной компоненты вектора скорости
+        }
+
+        if (vertical != 0) // проверка вертикальной оси
+        {
+            if (vertical > 0) // если нажата кнопка вверх
+            {
+                vel = speed; //модуль вектора скорости равен скорости
+            }
+
+
+            else if (vertical < 0) // если нажата кнопка вниз
+            {
+                vel = -speed; //модуль вектора скорости равен -скорости
+            }
+            verMove += new Vector3(-vel * (float)Math.Cos(ang), 0, vel * (float)Math.Sin(ang)) * Time.deltaTime; //вчислеие трёхмерной вертикальной компоненты вектора скорости
+        }
+
+        movement = horMove + verMove; // сложение трёхмерных компонент скоростей
+
+        if (Input.GetKey("q")) // если нажата кнопка q
+        {
+            rig.MoveRotation(rig.rotation * angvelL); //повернуть rigidbody против часовой
+
+
+        }
+        if (Input.GetKey("e")) // если нажата кнопка e
+        {
+            rig.MoveRotation(rig.rotation * angvelR); //повернуть rigidbody по часовой
+        }
+
+        rig.MovePosition(transform.position + movement); // перемещение обекта
+
+
+        //Quaternion camRotation = Quaternion.Euler(30, rig.rotation.eulerAngles.y - 90, 0); // определение угла поворота камеры относительно объекта
+
+        //cam.transform.rotation = camRotation; // поворот камеры
+
+        //offset.x = Mathf.Sin(cam.transform.eulerAngles.y * Mathf.PI / 180) * -camDistance; //тригонометрия 9 класс школы
+        //offset.z = Mathf.Cos(cam.transform.eulerAngles.y * Mathf.PI / 180) * -camDistance; //тригонометрия 9 класс школы
+        //offset.y = camDistance; // отдаление камеры
+
+        //cam.transform.position = rig.position + offset; //подтаскивание камеры к нужным координатам
+    }
+}
---
+++ alMouseCam.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alMouseCam : MonoBehaviour
+{
+    public Camera cam; // камера
+    public Transform transform;
+   // private Vector3 offset; // смещение камеры относительно объекта
+    private Vector3 camRotation;
+    private Vector3 lastPos;
+    public float camDistance = 10f; //отдаление камеры
+    private Vector2 prevMousePos;
+    public float camSpeed = 1f;
+    private float roundSpeed = 4.6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevMousePos.x = Input.mousePosition.x;
+        prevMousePos.y = Input.mousePosition.y;
+        camRotation = new Vector3(0, transform.rotation.y, 0);
+        cam.transform.Rotate(new Vector3(0, 0, 0), Space.World);
+        lastPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos;
+        mousePos.x = Input.mousePosition.x;
+        mousePos.y = Input.mousePosition.y;
+
+        cam.transform.LookAt(transform);
+        if (Input.GetMouseButton(0))
+        { 
+            if (mousePos.x - prevMousePos.x != 0)
+            {
+                cam.transform.RotateAround(transform.position, new Vector3(0, 1, 0), camSpeed * (mousePos.x - prevMousePos.x) * Time.deltaTime);
+            }
+
+            if (mousePos.y - prevMousePos.y != 0)
+            {
+                cam.transform.RotateAround(transform.position, new Vector3(-Mathf.Cos(cam.transform.eulerAngles.y * Mathf.PI / 180), 0, Mathf.Sin(cam.transform.eulerAngles.y * Mathf.PI / 180)), camSpeed * (mousePos.y - prevMousePos.y) * Time.deltaTime);
+            }
+
+        }
+
+
+        if (Input.GetKey("e"))
+        {
+            cam.transform.RotateAround(transform.position, new Vector3(0, 1, 0), camSpeed * roundSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("q"))
+        {
+            cam.transform.RotateAround(transform.position, new Vector3(0, -1, 0), camSpeed * roundSpeed * Time.deltaTime);
+        }
+
+        float dist = Vector3.Distance(cam.transform.position, transform.position);
+
+        //if (dist != camDistance || dist != camDistance)
+        //{
+        //    Vector3 offset = new Vector3(0, 0, 0);
+        //    //offset.y += transform.position.y - cam.transform.position.y + camDistance;
+        //    //offset.z += transform.position.z - cam.transform.position.z + camDistance;
+        //    cam.transform.position += offset;
+        //}
+        cam.transform.position += transform.position - lastPos;
+        lastPos = transform.position;
+
+
+        //cam.transform.Translate();
+
+        //offset.x = Mathf.Sin(cam.transform.eulerAngles.y * Mathf.PI / 180) * -camDistance; //тригонометрия 9 класс школы
+        //offset.z = Mathf.Cos(cam.transform.eulerAngles.y * Mathf.PI / 180) * -camDistance; //тригонометрия 9 класс школы
+       // offset.y = camDistance; // отдаление камеры
+
+       // cam.transform.position = transform.position + offset; //подтаскивание камеры к нужным координатам
+
+        prevMousePos.x = mousePos.x;
+        prevMousePos.y = mousePos.y;
+    }
+}
